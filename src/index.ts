06e19ba1911b7098459b8f5631d048d68a35bf66
rygenzx <maulana.zx525@gmail.com>
2021-10-28 16:54:24 +0700

    fix: set env in heroku

--- src/index.ts
+++ src/index.ts
@@ -146,12 +146,3 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  const path = require('path');
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
