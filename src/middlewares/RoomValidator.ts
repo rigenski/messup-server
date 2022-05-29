acbfc0e2a78e5e69b762effc78dabf6f667dbd8e
rygenzx <maulana.zx525@gmail.com>
2022-05-29 15:52:38 +0700

    Heroku

--- src/middlewares/RoomValidator.ts
+++ src/middlewares/RoomValidator.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import { check, validationResult } from 'express-validator';
 
 const create = [
-  check('name').isString().isLength({ max: 16 }),
+  check('name').isString().isLength({ max: 12 }),
   (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
 
